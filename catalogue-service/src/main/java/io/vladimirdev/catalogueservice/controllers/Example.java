39ea9888c9a3ed06e5861a82402d2e350b3c9701
PapaKarlos97 <97maksimov97@gmail.com>
2024-09-06 05:36:22 +0300

    merge secondFord and master

---
+++ catalogue-service/src/main/java/io/vladimirdev/catalogueservice/controllers/Example.java
@@ -0,0 +1,10 @@
+package io.vladimirdev.catalogueservice.controllers;
+
+public class Example {
+    public static void main(String[] args) {
+        for (int i = 0; i <100 ; i++) {
+
+            System.out.println(i);
+        }
+    }
+}
